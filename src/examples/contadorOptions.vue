b319b9fddaf40401a01967f49eeba78689c54798
unknown <companydams@gmail.com>
2024-01-08 05:34:20 -0500

    Add basic examples with options API and Composition API

---
+++ src/examples/contadorOptions.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 100,
+        };
+    },
+    methods: {
+        decresase() {
+            this.count -= 1; //this.count = this.count - 1;
+        }
+    },
+    mounted() {
+        console.log('El valor inicial del contador es ' - this.count);
+    },
+}
+</script>
+
+<template>
+    <h1>Contador: {{ count }}</h1>
+    <button @click="decresase">
+        Disminuir contador
+    </button>
+</template>
---
+++ src/examples/mainOptions.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+
+import contador from './contador.vue'
+
+const app = createApp(contador)
+
+app.mount('#app')
