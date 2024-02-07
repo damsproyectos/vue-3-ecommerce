11af256a55280377a56ff4bc63616ec40a87d0bd
unknown <companydams@gmail.com>
2024-02-07 04:47:52 -0500

    Move json files to src/data if we want to import

--- src/stores/categories.ts
+++ src/stores/categories.ts
@@ -1,21 +1,12 @@
 import { defineStore } from "pinia"
 import type { Category } from "@/model/types";
+import categoriesData from '../data/categories.json'
+
+// console.log(categoriesData); ////Agregamos éste console log para verificar cuál es el contenido de categoriesData; para ver si realmente estamos leyendo bien. Osea verificar si está llegando a nuestro Store categories.ts
 
 export const useCategoriesStore = defineStore('categories', {
     state: () => ({
-        categories: [{
-            id: 1,
-            name: 'Oficina',
-            description: 'Productos para tu oficina'
-        }, {
-            id: 2,
-            name: 'Computadora',
-            description: 'Accesorios para computadora'
-        }, {
-            id: 3,
-            name: 'Servicios',
-            description: 'Servicios profesionales'
-        }] as Category []
+        categories: categoriesData as Category []
     }),
     getters: {
         
